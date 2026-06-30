ae4581a9aaa7a725974cea3b82b4bfb0115ba3ce
SpottedToad <deepspacekind@gmail.com>
2026-06-29 20:45:16 -0600

    lets hope i didnt just break a bunch of stuff

---
+++ Toads Simple Mod/Fabric-TSOMod-1.21.1/src/main/java/net/spottedtoad/toads_simple_origns/TSOMod.java
@@ -0,0 +1,18 @@
+package net.spottedtoad.toads_simple_origns;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.ResourceLocation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TSOMod implements ModInitializer {
+	public static final String MOD_ID = "toads_simple_origins";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ Toads Simple Mod/Fabric-TSOMod-1.21.1/src/main/java/net/spottedtoad/toads_simple_origns/TSOModClient.java
@@ -0,0 +1,10 @@
+package net.spottedtoad.toads_simple_origns;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TSOModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
