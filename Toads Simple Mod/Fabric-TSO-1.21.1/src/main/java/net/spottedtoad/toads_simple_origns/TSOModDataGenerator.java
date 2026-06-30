a73d1df89052a9eb9aa60b6ab54a956fc2c0b828
SpottedToad <deepspacekind@gmail.com>
2026-06-29 20:36:40 -0600

    created github reposit

---
+++ Toads Simple Mod/Fabric-TSO-1.21.1/src/main/java/net/spottedtoad/toads_simple_origns/TSOModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.spottedtoad.toads_simple_origns;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Toads_simple_originsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
